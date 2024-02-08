b36d61e69da179cb82d58fc17715812e68f47b50
abrahanalarcon <moiseshazim@gmail.com>
2024-02-08 12:11:54 -0400

    primer paso de lista de gastos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import ListadoGastos from './components/ListadoGastos'
 import Modal from './components/Modal'
 import { generarId } from './helpers'
 import IconoNuevoGasto from './img/nuevo-gasto.svg'
@@ -27,6 +28,7 @@ function App() {
 
   const guardarGasto = gasto => {
     gastos.id = generarId();
+    gasto.fecha = Date.now();
     setGastos([...gastos, gasto]);
 
     
@@ -49,13 +51,21 @@ function App() {
       />
        
       {isValidPresupuesto && (
-      <div className="nuevo-gasto">
+      <>
+         <main>
+           <ListadoGastos
+            gastos={gastos}
+           />
+         </main>
+         <div className="nuevo-gasto">
            <img
                src={IconoNuevoGasto}
                alt="icono nuevo gasto"
                onClick={handleNuevoGasto}
            />
-      </div>
+         </div>
+      </>
+      
       )}
 
       {modal && <Modal 
